13c048ce56ff83f87dda612928e1a78ab14f4b56
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-07-05 18:18:20 +0000

    Sync LeetCode submission - Find Lucky Integer in an Array - Runtime - 0 ms (100.00%), Memory - 14 MB (57.85%)

---
+++ LeetCode/1510-find-lucky-integer-in-an-array/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int, int> freq;
+
+        // Count frequency of each number
+        for (int num : arr) {
+            freq[num]++;
+        }
+
+        int result = -1;
+
+        // Check if any number's frequency equals its value
+        for (auto& [num, count] : freq) {
+            if (num == count) {
+                result = max(result, num);
+            }
+        }
+
+        return result;
+    }
+};
+
