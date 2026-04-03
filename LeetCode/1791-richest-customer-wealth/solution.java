5a6ec1dda10e61408ad2541e4f19dede1e2052ef
Nidhi Kumari <nidhigupta2462@gmail.com>
2026-04-03 16:15:22 +0000

    Sync LeetCode submission - Richest Customer Wealth - Runtime - 0 ms (100.00%), Memory - 44.6 MB (37.27%)

--- LeetCode/1791-richest-customer-wealth/solution.java
+++ LeetCode/1791-richest-customer-wealth/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-      int result = 0;
-      for (int i =0; i< accounts.length; i++) {
-        int sum =0;
-        for (int j =0;j<accounts [i].length;j++){
-            sum += accounts [i][j];
+        int result = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            result = Math.max(result, sum);
         }
-        result = Math.max(result,sum);
-      }
-      return result;
+        return result;
     }
 }
