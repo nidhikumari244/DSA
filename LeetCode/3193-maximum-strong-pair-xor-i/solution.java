ec4dfa8a9beffd6c7664dae4c9e994d4227dbe2d
Nidhi Kumari <nidhigupta2462@gmail.com>
2026-07-17 19:25:17 +0000

    Sync LeetCode submission - Maximum Strong Pair XOR I - Runtime - 2 ms (99.62%), Memory - 44.8 MB (81.25%)

---
+++ LeetCode/3193-maximum-strong-pair-xor-i/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumStrongPairXor(int[] nums) {
+        int maxXor = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+
+                int x = nums[i];
+                int y = nums[j];
+
+                if (Math.abs(x - y) <= Math.min(x, y)) {
+                    maxXor = Math.max(maxXor, x ^ y);
+                }
+            }
+        }
+
+        return maxXor;
+    }
+}
