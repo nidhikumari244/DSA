03fc739d0e6e9d0cae43ee06cbe7c6fc053f6d5e
Nidhi Kumari <nidhigupta2462@gmail.com>
2026-07-30 18:34:46 +0000

    Sync LeetCode submission - Convert a Number to Hexadecimal - Runtime - 0 ms (100.00%), Memory - 42.6 MB (45.82%)

---
+++ LeetCode/0405-convert-a-number-to-hexadecimal/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String toHex(int num) {
+
+        if (num == 0) {
+            return "0";
+        }
+
+        String hex = "0123456789abcdef";
+        StringBuilder ans = new StringBuilder();
+
+        while (num != 0) {
+
+            ans.append(hex.charAt(num & 15));
+
+            num >>>= 4;
+        }
+
+        return ans.reverse().toString();
+    }
+}
