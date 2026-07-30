0283d665c22b2fa672068476dcb8ba87ecef12ac
Nidhi Kumari <nidhigupta2462@gmail.com>
2026-07-30 18:33:32 +0000

    Sync LeetCode submission - Binary Tree Paths - Runtime - 4 ms (72.97%), Memory - 49.5 MB (52.57%)

---
+++ LeetCode/0257-binary-tree-paths/solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+
+        List<String> ans = new ArrayList<>();
+
+        dfs(root, "", ans);
+
+        return ans;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> ans) {
+
+        if (node == null) {
+            return;
+        }
+
+        if (path.length() == 0) {
+            path = String.valueOf(node.val);
+        } else {
+            path += "->" + node.val;
+        }
+
+        if (node.left == null && node.right == null) {
+            ans.add(path);
+            return;
+        }
+
+        dfs(node.left, path, ans);
+        dfs(node.right, path, ans);
+    }
+}
