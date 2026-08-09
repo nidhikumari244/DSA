1ccc8f0c573a17db6b822f8fbfd3cbedc8ae40da
Nidhi Kumari <nidhigupta2462@gmail.com>
2026-08-09 18:48:54 +0000

    Sync LeetCode submission - Valid Parentheses - Runtime - 3 ms (86.02%), Memory - 43.2 MB (58.08%)

---
+++ LeetCode/0020-valid-parentheses/solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            // Closing brackets
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        // All opening brackets must be matched
+        return stack.isEmpty();
+    }
+}
