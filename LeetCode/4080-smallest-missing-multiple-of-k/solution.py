68a02c34e0b4beba0b8a3249e7fe8b20ecbf5ad3
Nidhi Kumari <nidhigupta2462@gmail.com>
2026-08-25 19:29:18 +0000

    Sync LeetCode submission - Smallest Missing Multiple of K - Runtime - 0 ms (100.00%), Memory - 12.4 MB (55.87%)

---
+++ LeetCode/4080-smallest-missing-multiple-of-k/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingMultiple(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        s = set(nums)
+        m = k
+        while m in s:
+            m += k
+        return m
