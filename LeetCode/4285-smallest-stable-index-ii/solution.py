ea91640ed9fb445ba594b48d09b318e5fb942722
Nidhi Kumari <nidhigupta2462@gmail.com>
2026-09-05 18:01:11 +0000

    Sync LeetCode submission - Smallest Stable Index II - Runtime - 299 ms (80.11%), Memory - 22.1 MB (31.63%)

---
+++ LeetCode/4285-smallest-stable-index-ii/solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        n = len(nums)
+
+        # suffix_min[i] = minimum from i to n-1
+        suffix_min = [0] * n
+
+        suffix_min[n - 1] = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i + 1])
+
+        # Find first stable index
+        prefix_max = nums[0]
+
+        for i in range(n):
+            prefix_max = max(prefix_max, nums[i])
+
+            if prefix_max - suffix_min[i] <= k:
+                return i
+
+        return -1
