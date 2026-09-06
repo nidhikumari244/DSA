9eb0e873e20bf596ea309f90acbdd9d30c3366ac
Nidhi Kumari <nidhigupta2462@gmail.com>
2026-09-06 18:51:32 +0000

    Sync LeetCode submission - Distinct Subsequences - Runtime - 491 ms (49.13%), Memory - 69.4 MB (9.47%)

---
+++ LeetCode/0115-distinct-subsequences/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def numDistinct(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+
+        m = len(s)
+        n = len(t)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        # Empty t can be formed in exactly 1 way
+        for i in range(m + 1):
+            dp[i][n] = 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
+                else:
+                    dp[i][j] = dp[i + 1][j]
+
+        return dp[0][0]
